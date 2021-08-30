95ebde3ef7c34de3f29db4ccb2f934e890f1db24
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-08-29 22:51:05 -0300

    Create readKyInHeaderFits.cpp
    Como compilar - gcc -o returnKey returnKey.cpp -L. -lcfitsio -lm -lnsl 
    
    como executar 
    ./returnKey arquivo.fits[PosiçãoHeader] "NomedaChave"

---
+++ readKyInHeaderFits.cpp
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <stdio.h>
+#include <string.h>
+#include <fitsio.h>
+
+
+void isPartOf(char *, char *);
+
+    int main(int argc, char *argv[])
+    {
+      fitsfile *fptr;         
+        char card[FLEN_CARD]; 
+        char *compararkey=argv[2];
+        
+
+      int status = 0,  nkeys, ii; 
+
+      fits_open_file(&fptr, argv[1], READONLY, &status);
+        fits_get_hdrspace(fptr, &nkeys, NULL, &status);
+
+        
+        for (ii = 1; ii <= nkeys; ii++)  { 
+          fits_read_record(fptr, ii, card, &status); 
+          
+          if(strstr(card,compararkey)!=NULL){
+
+          printf("%s\n", card);
+
+          }
+         
+        }
+        
+        fits_close_file(fptr, &status);
+
+        if (status)          /* print any error messages */
+          fits_report_error(stderr, status);
+        return(status);
+    }
